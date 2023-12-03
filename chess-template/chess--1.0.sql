cf70a98c3c390cd5608aff1f8f91853124c05174
Gian <gian@Gians-MBP.lan>
2023-12-03 19:56:05 +0100

    datatypes

--- chess-template/chess--1.0.sql
+++ chess-template/chess--1.0.sql
@@ -1,48 +1,61 @@
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION chess" to load this file. \quit
-
-/******************************************************************************
- * Input/Output
- ******************************************************************************/
-
-CREATE OR REPLACE FUNCTION chessgame_in(cstring)
-  RETURNS chessgame
-  AS 'MODULE_PATHNAME'
-  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
-CREATE OR REPLACE FUNCTION chessgame_out(chessgame)
-  RETURNS cstring
-  AS 'MODULE_PATHNAME'
-  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
-CREATE OR REPLACE FUNCTION chessgame_recv(internal)
-  RETURNS chessgame
-  AS 'MODULE_PATHNAME'
-  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
-CREATE OR REPLACE FUNCTION chessgame_send(chessgame)
-  RETURNS bytea
-  AS 'MODULE_PATHNAME'
-  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-
-CREATE TYPE chessgame (
-  internallength = 512,
-  input          = chessgame_in,
-  output         = chessgame_out,
-  receive        = chessgame_recv,
-  send           = chessgame_send,
-  alignment      = char
+CREATE TYPE chessboard AS (
+    fen text
 );
 
-CREATE OR REPLACE FUNCTION chessgame(text)
-  RETURNS chessgame
-  AS 'MODULE_PATHNAME', 'chessgame_cast_from_text'
-  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+CREATE TYPE chessgame AS (
+    san text
+);
 
-CREATE OR REPLACE FUNCTION text(chessgame)
-  RETURNS text
-  AS 'MODULE_PATHNAME', 'chessgame_cast_to_text'
-  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+CREATE OR REPLACE FUNCTION chessgame_in(cstring)
+    RETURNS chessgame
+    AS 'MODULE_PATHNAME', 'chessgame_in'
+    LANGUAGE C STRICT;
 
-CREATE CAST (text as chessgame) WITH FUNCTION chessgame(text) AS IMPLICIT;
-CREATE CAST (chessgame as text) WITH FUNCTION text(chessgame);
+CREATE OR REPLACE FUNCTION chessgame_out(chessgame)
+    RETURNS cstring
+    AS 'MODULE_PATHNAME', 'chessgame_out'
+    LANGUAGE C STRICT;
+
+CREATE OR REPLACE FUNCTION chessgame_recv(internal)
+    RETURNS chessgame
+    AS 'MODULE_PATHNAME', 'chessgame_recv'
+    LANGUAGE C STRICT;
+
+CREATE OR REPLACE FUNCTION chessgame_send(chessgame)
+    RETURNS bytea
+    AS 'MODULE_PATHNAME', 'chessgame_send'
+    LANGUAGE C STRICT;
+
+CREATE OR REPLACE FUNCTION chessboard_in(cstring)
+    RETURNS chessboard
+    AS 'MODULE_PATHNAME', 'chessboard_in'
+    LANGUAGE C STRICT;
+
+CREATE OR REPLACE FUNCTION chessboard_out(chessboard)
+    RETURNS cstring
+    AS 'MODULE_PATHNAME', 'chessboard_out'
+    LANGUAGE C STRICT;
+
+CREATE OR REPLACE FUNCTION chessboard_recv(internal)
+    RETURNS chessboard
+    AS 'MODULE_PATHNAME', 'chessboard_recv'
+    LANGUAGE C STRICT;
+
+CREATE OR REPLACE FUNCTION chessboard_send(chessboard)
+    RETURNS bytea
+    AS 'MODULE_PATHNAME', 'chessboard_send'
+    LANGUAGE C STRICT;
+
+    
+
+CREATE OR REPLACE FUNCTION hasBoard(chessgame, chessboard, integer) 
+    RETURNS bool
+    AS 'MODULE_PATHNAME', 'hasBoard'
+    LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;  
+
+CREATE OR REPLACE FUNCTION check_board_state(game_moves text, board_state text, half_moves integer)
+RETURNS boolean AS $$
+BEGIN
+    RETURN hasBoard(game_moves::chessgame, board_state::chessboard, half_moves);
+END;
+$$ LANGUAGE plpgsql;
--- chess-template/chess.c
+++ chess-template/chess.c
@@ -1,115 +1,275 @@
 #include <stdio.h>
 #include <postgres.h>
 #include <stdlib.h>
+#include <fmgr.h>
 
 #include "utils/builtins.h"
 #include "libpq/pqformat.h"
-
 #include "smallchesslib.h"
+#include <stdbool.h>
+#include <string.h>
+
+#define ERR_CODE1    400
+#define ERR_CODE2    401
+// #ifndef CHESS_FUNCTIONS_H
+// #define CHESS_FUNCTIONS_H
+
 
 PG_MODULE_MAGIC;
 
-/*****************************************************************************/
+typedef struct {
+    char fen[128]; // FEN representation of chessboard
+} chessboard;
 
-#define DatumGetChessgameP(X)  ((SCL_Record *) DatumGetPointer(X))
+typedef struct {
+    char san[128]; // SAN representation of chess game
+} chessgame;
+
+
+bool smallchesslib_is_valid_fen(const char *fen) {
+    // Implementation to validate FEN notation
+    int rank = 0;
+    int file = 0;
+    int piecesCount = 0;
+
+    while (*fen && rank < 8) {
+        if (*fen == '/') {
+            if (file != 8) {
+                // Invalid rank length
+                return false;
+            }
+            file = 0;
+            rank++;
+        } else if (isdigit(*fen)) {
+            file += *fen - '0';
+            if (file > 8) {
+                // Invalid file length
+                return false;
+            }
+        } else if (strchr("KQRBNPkqrbnp", *fen)) {
+            file++;
+            piecesCount++;
+            if (file > 8) {
+                // Invalid file length
+                return false;
+            }
+        } else {
+            // Invalid character
+            return false;
+        }
+        fen++;
+    }
+
+    if (rank != 8 || piecesCount < 2) {
+        // FEN should have 8 ranks and at least two pieces (both sides)
+        return false;
+    }
+
+    // Check additional FEN conditions if required
+
+    return true;
+}
+
+#define DatumGetChessgameP(X)  ((chessgame *) DatumGetPointer(X))
 #define ChessgamePGetDatum(X)  PointerGetDatum(X)
 #define PG_GETARG_CHESSGAME_P(n) DatumGetChessgameP(PG_GETARG_DATUM(n))
 #define PG_RETURN_CHESSGAME_P(x) return ChessgamePGetDatum(x)
 
-/*****************************************************************************/
+#define DatumGetChessboardP(X)  ((chessboard *) DatumGetPointer(X))
+#define ChessboardPGetDatum(X)  PointerGetDatum(X)
+#define PG_GETARG_CHESSBOARD_P(n) DatumGetChessboardP(PG_GETARG_DATUM(n))
 
-char str[4096];
+/*___________________________________________________________________________
+* Function declarations */
 
-void putCharStr(char c)
-{
-    char* s = str;
+int getBoard(chessgame record, int moves);
+int getFirstMoves(chessgame r, int moves);
+int hasOpening(chessgame record, chessgame opening);    
+//int hasBoard(chessgame record, chessgame board, int moves);
 
-    while (*s != 0)
-        s++;
-
-    *s = c;
-    *(s + 1) = 0;
-}
-
-/*****************************************************************************/
-
-static SCL_Record *
-chessgame_make(const char *string)
-{
-  SCL_Record *record = palloc0(sizeof(SCL_Record));
-  SCL_recordFromPGN(record, string);
-  return record;
-}
-
-static char *
-chessgame_to_str(const SCL_Record *record)
-{
-  char* s = str;
-  *s = 0;
-  SCL_printPGN(record, putCharStr, 0);
-  return str;
-}
-
-/*****************************************************************************/
 
 PG_FUNCTION_INFO_V1(chessgame_in);
-Datum
-chessgame_in(PG_FUNCTION_ARGS)
-{
-  char *str = PG_GETARG_CSTRING(0);
-  PG_RETURN_CHESSGAME_P(chessgame_make(&str));
-}
-
 PG_FUNCTION_INFO_V1(chessgame_out);
-Datum
-chessgame_out(PG_FUNCTION_ARGS)
-{
-  SCL_Record *record = PG_GETARG_CHESSGAME_P(0);
-  char *result = chessgame_to_str(record);
-  PG_FREE_IF_COPY(record, 0);
-  PG_RETURN_CSTRING(result);
-}
-
 PG_FUNCTION_INFO_V1(chessgame_recv);
-Datum
-chessgame_recv(PG_FUNCTION_ARGS)
-{
-  StringInfo  buf = (StringInfo) PG_GETARG_POINTER(0);
-  SCL_Record *record = (SCL_Record *) palloc(sizeof(SCL_Record));
-  record = chessgame_make(pq_getmsgstring(buf));
-  PG_RETURN_CHESSGAME_P(record);
-}
-
 PG_FUNCTION_INFO_V1(chessgame_send);
-Datum
-chessgame_send(PG_FUNCTION_ARGS)
-{
-  SCL_Record *record = PG_GETARG_CHESSGAME_P(0);
-  StringInfoData buf;
-  pq_begintypsend(&buf);
-  int len = SCL_recordLength(record);
-  pq_sendtext(&buf, record, len);
-  PG_FREE_IF_COPY(record, 0);
-  PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
+
+PG_FUNCTION_INFO_V1(chessboard_in);
+PG_FUNCTION_INFO_V1(chessboard_out);
+PG_FUNCTION_INFO_V1(chessboard_recv);
+PG_FUNCTION_INFO_V1(chessboard_send);
+
+
+
+// Function to convert external representation to chessgame type
+Datum chessgame_in(PG_FUNCTION_ARGS) {
+    char *str = PG_GETARG_CSTRING(0);
+    chessgame *result;
+
+    // Create a function to parse the input string into the expected format
+    // Example logic (modify as needed) - assuming moves separated by space
+    char sanitized_moves[128] = ""; // Adjust the size as needed
+    char move[16]; // Adjust the size as needed
+    int index = 0;
+
+    // Iterate through the input string to format it correctly for chessgame
+    while (*str != '\0') {
+        if (*str == ' ' && index > 0) {
+            sanitized_moves[index++] = ' ';
+        } else if (isalnum(*str) || *str == '-') {
+            sanitized_moves[index++] = *str;
+        }
+        str++;
+    }
+    sanitized_moves[index] = '\0';
+
+    result = (chessgame *) palloc(sizeof(chessgame));
+    strncpy(result->san, sanitized_moves, sizeof(result->san));
+    PG_RETURN_CHESSGAME_P(result);
 }
 
-PG_FUNCTION_INFO_V1(chessgame_cast_from_text);
-Datum
-chessgame_cast_from_text(PG_FUNCTION_ARGS)
-{
-  text *txt = PG_GETARG_TEXT_P(0);
-  char *str = DatumGetCString(DirectFunctionCall1(textout,
-               PointerGetDatum(txt)));
-  PG_RETURN_CHESSGAME_P(chessgame_make(&str));
+
+// Function to convert chessgame type to external representation
+Datum chessgame_out(PG_FUNCTION_ARGS) {
+    chessgame *chess = PG_GETARG_CHESSGAME_P(0);
+    char *result;
+
+    result = psprintf("%s", chess->san);
+    PG_RETURN_CSTRING(result);
 }
 
-PG_FUNCTION_INFO_V1(chessgame_cast_to_text);
-Datum
-chessgame_cast_to_text(PG_FUNCTION_ARGS)
-{
-  SCL_Record *record  = PG_GETARG_CHESSGAME_P(0);
-  text *out = (text *)DirectFunctionCall1(textin,
-            PointerGetDatum(chessgame_to_str(record)));
-  PG_FREE_IF_COPY(record, 0);
-  PG_RETURN_TEXT_P(out);
+// Function to receive chessgame type from binary representation
+Datum chessgame_recv(PG_FUNCTION_ARGS) {
+    ereport(ERROR,
+            (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+             errmsg("chessgame_recv not implemented")));
+    PG_RETURN_NULL();
+}
+
+// Function to send chessgame type as binary representation
+Datum chessgame_send(PG_FUNCTION_ARGS) {
+    ereport(ERROR,
+            (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+             errmsg("chessgame_send not implemented")));
+    PG_RETURN_NULL();
+}
+
+// Function to convert external representation to chessboard type
+Datum chessboard_in(PG_FUNCTION_ARGS) {
+    char *fen_str = PG_GETARG_CSTRING(0);
+    chessboard *result = (chessboard *) palloc(sizeof(chessboard));
+
+    // Use functions from smallchesslib to validate and handle FEN string
+    if (!smallchesslib_is_valid_fen(fen_str)) {
+        ereport(ERROR,
+                (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+                 errmsg("Invalid FEN string")));
+        PG_RETURN_NULL();
+    }
+
+    // Copy the validated FEN string
+    strncpy(result->fen, fen_str, sizeof(result->fen));
+    PG_RETURN_POINTER(result);
+}
+
+// Function to convert chessboard type to external representation
+Datum chessboard_out(PG_FUNCTION_ARGS) {
+    chessboard *board = (chessboard *) PG_GETARG_POINTER(0);
+    char *result;
+
+    result = psprintf("%s", board->fen);
+    PG_RETURN_CSTRING(result);
+}
+
+// Function to receive chessboard type from binary representation
+Datum chessboard_recv(PG_FUNCTION_ARGS) {
+    ereport(ERROR,
+            (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+             errmsg("chessboard_recv not implemented")));
+    PG_RETURN_NULL();
+}
+Datum chessboard_send(PG_FUNCTION_ARGS) {
+    ereport(ERROR,
+            (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+             errmsg("chessboard_send not implemented")));
+    PG_RETURN_NULL();
+}
+
+/*___________________________________________________________________________
+* Function aditionals */
+
+void getBoardStateFromGame(chessgame *game, int move_index, chessboard *board) {
+    // Mock logic to simulate extracting board state based on move index
+    snprintf(board->fen, sizeof(board->fen), "FEN at move %d", move_index);
+}
+
+// Function to convert the chessboard type to a string representation
+void convertChessboardToString(chessboard *board, char *board_str) {
+    // Mock logic to convert the chessboard to a string (FEN notation in this example)
+    strncpy(board_str, board->fen, sizeof(board->fen));
+}
+
+PG_FUNCTION_INFO_V1(hasBoard);
+Datum hasBoard(PG_FUNCTION_ARGS) {
+    if (PG_ARGISNULL(0) || PG_ARGISNULL(1) || PG_ARGISNULL(2)) {
+        ereport(ERROR,
+                (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+                 errmsg("Invalid arguments for hasBoard")));
+        PG_RETURN_NULL();
+    }
+
+    chessgame *game = PG_GETARG_CHESSGAME_P(0);
+    text *board_text = PG_GETARG_TEXT_P(1);
+    int halfMoves = PG_GETARG_INT32(2);
+
+    // Convert text to char * for chessboard
+    char *board_str = text_to_cstring(board_text);
+
+    // Create a chessboard structure and set the FEN
+    chessboard *board = (chessboard *) palloc(sizeof(chessboard));
+    strncpy(board->fen, board_str, sizeof(board->fen));
+
+    // Free the memory allocated by text_to_cstring
+    pfree(board_str);
+
+    // Create a temporary chessboard to apply moves and compare states
+    chessboard tempBoard;
+    strncpy(tempBoard.fen, board->fen, sizeof(tempBoard.fen));
+
+    // Parse the moves from the chessgame
+    char *moves = strtok(game->san, " ");
+    int moveCount = 0;
+
+    while (moves != NULL && moveCount < halfMoves * 2) {
+        // Apply each move to the temporary board
+        char tempFEN[128]; // Temporary FEN to update the board state
+        strncpy(tempFEN, tempBoard.fen, sizeof(tempFEN)); // Copy the current board state
+
+        // Extract the source and destination squares from the move
+        char from[3], to[3];
+        strncpy(from, moves, 2);
+        from[2] = '\0';
+        strncpy(to, moves + 2, 2);
+        to[2] = '\0';
+
+        // Find the source and destination indexes in FEN notation
+        int sourceIndex = 56 - ((from[1] - '1') * 8) + (from[0] - 'a');
+        int destIndex = 56 - ((to[1] - '1') * 8) + (to[0] - 'a');
+
+        // Move the piece in the FEN notation
+        tempFEN[destIndex] = tempFEN[sourceIndex];
+        tempFEN[sourceIndex] = '-';
+
+        // Update the temporary board state
+        strncpy(tempBoard.fen, tempFEN, sizeof(tempBoard.fen));
+
+        // Check if the temporary board matches the given board
+        if (strncmp(tempBoard.fen, board->fen, sizeof(tempBoard.fen)) == 0) {
+            PG_RETURN_BOOL(true); // Board state found in the chessgame
+        }
+
+        moves = strtok(NULL, " ");
+        moveCount++;
+    }
+
+    PG_RETURN_BOOL(false); // Board state not found in the specified half-moves
 }
